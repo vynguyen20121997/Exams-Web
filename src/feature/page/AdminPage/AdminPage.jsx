966635093d232a72c8c13e2e341447e82be5e9d3
HungTrinhIT <minhhung.it.work@gmail.com>
2023-12-22 20:04:46 +0700

    feature: update API login

--- src/feature/page/AdminPage/AdminPage.jsx
+++ src/feature/page/AdminPage/AdminPage.jsx
@@ -1,27 +1,26 @@
-import React from "react";
-import { SimpleFooter } from "../../../components/Footer/Footer";
-import DataTable from "./components/DataTable/DataTable";
-import { Logo } from "../../../assets/Logo/Logo";
+import React from 'react';
+import { SimpleFooter } from '../../../components/Footer/Footer';
+import DataTable from './components/DataTable/DataTable';
+import { Logo } from '../../../assets/Logo/Logo';
 
 const AdminPage = () => {
   return (
-    <div className="background-website-admin">
-      <div className="bg-white w-11/12 mx-auto rounded-3xl pb-5 ">
+    <div className='background-website-admin'>
+      <div className='bg-white w-11/12 mx-auto rounded-3xl pb-5 '>
         <div
-          className="bg-purple rounded-t-3xl h-20 flex w-full flex-row pt-4
-        justify-between px-20"
-        >
+          className='bg-purple rounded-t-3xl h-20 flex w-full flex-row pt-4
+        justify-between px-20'>
           <div>
-            <Logo textcolor="text-white" logocolor="white" />
+            <Logo textcolor='text-white' logocolor='white' />
           </div>
           <div></div>
         </div>
 
-        <div className="px-20 pt-5 pb-10">
+        <div className='px-20 pt-5 pb-10'>
           <DataTable />
         </div>
 
-        <div className="px-20">
+        <div className='px-20'>
           <SimpleFooter />
         </div>
       </div>
--- src/services/AuthAPI.js
+++ src/services/AuthAPI.js
@@ -1,18 +1,18 @@
-import api from "./axiosInstance";
+import api from './axiosInstance';
 
 const AuthAPI = {
-    register: (body) => {
-        const url = '/signup';
-        return api.post(url, body)
-    },
-    login: (body) => {
-        const url = '/login';
-        return api.post(url, body)
-    },
-    fetchCurrentUser: () => {
-        const url = '/fetchcurentUser';
-        return api.get(url)
-    }
-}
+  register: (body) => {
+    const url = '/auth/signup';
+    return api.post(url, body);
+  },
+  login: (body) => {
+    const url = '/auth/login';
+    return api.post(url, body);
+  },
+  fetchCurrentUser: () => {
+    const url = '/auth/me';
+    return api.get(url);
+  },
+};
 
-export default AuthAPI
+export default AuthAPI;
