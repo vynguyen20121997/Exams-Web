8ed9b5e96adffb14e3b491d622493ba2ead989c3
vynguyen20121997 <133662722+vynguyen20121997@users.noreply.github.com>
2024-01-15 21:04:54 +0700

    update

--- src/services/AdminPage/GetlistAPI.js
+++ src/services/AdminPage/GetlistAPI.js
@@ -1,28 +1,16 @@
 import api from "../axiosInstance";
 
 export const getListUserAdminPage = {
-    getListUser: (token) => {
-        const url = '/users';
-        return api.get(url,  {
-            headers: {
-              'x-access-token': token, 
-            },
-          })
-    },
-    getListTeacher:  (token) => {
-        const url = '/users/teachers';
-        return api.get(url,  {
-            headers: {
-              'x-access-token': token, 
-            },
-          })
-    },
-    getListStudent: (token) => {
-        const url = '/users/students';
-        return api.get(url,  {
-            headers: {
-              'x-access-token': token, 
-            },
-          })
-    },
-}
+  getListUser: () => {
+    const url = "/users";
+    return api.get(url);
+  },
+  getListTeacher: () => {
+    const url = "/users/teachers";
+    return api.get(url);
+  },
+  getListStudent: () => {
+    const url = "/users/students";
+    return api.get(url);
+  },
+};
